34b4850021b5c9ec74ca4323d6293f302a9a3954
Søren <soren9els1@gmail.com>
2019-04-23 11:49:00 +0200

    Add iterations of Sut

--- tests/Microwave.Test.Integration/It02Sut.cs
+++ tests/Microwave.Test.Integration/It02Sut.cs
@@ -2,18 +2,14 @@ using NUnit.Framework;
 
 namespace Tests
 {
+    // System under test
+    // Iteration 01
     public class Tests
     {
-        // System under test
-        // Iteration 01
         [SetUp]
         public void Setup()
         {
         }
-
-
-
-
 
         [Test]
         public void Test1()
