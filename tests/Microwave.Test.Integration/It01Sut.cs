a489a3524e5943062cc5b021c36b3a8c4ec6ffc8
Suuren <soren9els1@gmail.com>
2019-04-29 19:37:27 +0200

    Update to Include some Buttom tests

--- tests/Microwave.Test.Integration/It01Sut.cs
+++ tests/Microwave.Test.Integration/It01Sut.cs
@@ -146,5 +146,61 @@ namespace Tests
         // Door Tets:
         // Further Cases:
 
+
+        // ButtonTests:
+        // related to sut
+        [TestCase(3, TestName = "Powerbuttom ShouldIncrementPower")]
+        public void TestBtnPower(int result)
+        {
+            _sutUI = new UserInterface(_PBtn, _TBtn, _SCBtn, _Door, _fakeDisplay, _fakeLight, _fakeCC);
+
+            var wasCalledCount = 0;
+            _PBtn.Pressed += (o, e) => wasCalledCount = ++wasCalledCount;
+            _TBtn.Pressed += (o, e) => wasCalledCount = ++wasCalledCount;
+            _SCBtn.Pressed += (o, e) => wasCalledCount = ++wasCalledCount;
+
+            _PBtn.Press();  // 1
+            _TBtn.Press();  // 2
+            _SCBtn.Press(); // 3
+
+
+//             _fakeDisplay.Received(1).ShowPower(Arg.Is<int>(50));                // 1 
+//             _fakeCC.Received(1).Stop();                                         // 3
+//             _fakeDisplay.Received(1).Clear();                                   // 4 
+//             _fakeLight.Received(1).TurnOn();                                    // 4
+            Assert.That(wasCalledCount, Is.EqualTo(result));
+        }
+        [TestCase(2,100, TestName = "Powerbuttom Increment ShouldResultInPowerLevel(100)")]
+        [TestCase(14, 700, TestName = "Powerbuttom Increment ShouldResultInPowerLevel(700)")]
+        [TestCase(15, 50, TestName = "Powerbuttom Increment ShouldOverflow ToPowerLevel(50)")]
+        public void TestBtnPowerLoop(int loop,int result)
+        {
+            _sutUI = new UserInterface(_PBtn, _TBtn, _SCBtn, _Door, _fakeDisplay, _fakeLight, _fakeCC);
+
+            for (int i = 0; i < loop; ++i)
+            {
+                _PBtn.Press(); // 1
+            }
+
+            // Asserts that ShowPower is called with : userInterface.powerlevel
+            _fakeDisplay.ShowPower(Arg.Is<int>(result));                // 1 
+        }
+
+
+        [TestCase(1, 1, TestName = "Timebuttom Increment ShouldResultInTimeMin(100)")]
+        [TestCase(13, 13, TestName = "Timebuttom Increment ShouldResultInTimeMin(13)")]
+        [TestCase(int.MaxValue, int.MaxValue, TestName = "Timebuttom Increment ShouldResultInTimeMin(100)")]
+        public void TestBtnTimeLoop(int loop, int resultMin)
+        {
+            _sutUI = new UserInterface(_PBtn, _TBtn, _SCBtn, _Door, _fakeDisplay, _fakeLight, _fakeCC);
+
+            for (int i = 0; i < loop; ++i)
+            {
+                _TBtn.Press(); // 1
+            }
+
+            // Asserts that ShowTime is called with : userInterface.time
+            _fakeDisplay.ShowTime(Arg.Is<int>(resultMin), Arg.Is<int>(0));  // 1
+        }
     }
 }
