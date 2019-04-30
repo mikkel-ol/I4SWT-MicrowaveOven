eb43c3af900663b7877e86ac5b0d0930cbb43a1d
Alexander Bjerre <alexander8@hotmail.com>
2019-04-30 18:58:57 +0200

    add test for power showing correctly when pressing power button one whole cycle

--- src/Microwave.Core/Boundary/PowerTube.cs
+++ src/Microwave.Core/Boundary/PowerTube.cs
@@ -16,9 +16,9 @@ namespace Microwave.Core.Boundary
 
         public void TurnOn(int power)
         {
-            if (power < 1 || 100 < power)
+            if (power < 50 || 700 < power)
             {
-                throw new ArgumentOutOfRangeException("power", power, "Must be between 1 and 100 % (incl.)");
+                throw new ArgumentOutOfRangeException("power", power, "Must be between 50 and 700 Watt (incl.)");
             }
 
             if (IsOn)
--- tests/Microwave.Test.Integration/It03Sut-1.cs
+++ tests/Microwave.Test.Integration/It03Sut-1.cs
@@ -43,13 +43,118 @@ namespace Microwave.Test.Integration
 
             _CookCtrl = new CookController(_Timer, _fakeDisp, _SutPowerTube);
             _UI = new UserInterface(_PBtn, _TBtn, _SCBtn, _Door, _fakeDisp, _Light, _CookCtrl);
+            _CookCtrl.UI = _UI;
+
+        }
+
+        // Test power ved forskellige klik og ingen klik på powerbutton og et press for mange, om den så går tilbage til 50 eller fortsætter op
+        // Test power ER  reset ved start-cancel klik
+        // Test power IKKE bliver reset ved open door
+        // Test power ER reset efter start-cancel klik under cooking state
+        // Test power ER reset efter open-door under cooking state
+        // Test power ved start-cancel, uden power? Kan man gøre som deres unit test exceptions?
+
+        [Test]
+        public void TurnOn_1PressOnPwrButton_PowerIs50()
+        {
+            _PBtn.Press();
+            _TBtn.Press();
+            _SCBtn.Press();
+
+            _fakeOut.Received().OutputLine(Arg.Is<string>(str => str.Contains("50")));
+        }
+
+        [Test]
+        public void TurnOn_2PressOnPwrButton_PowerIs100()
+        {
+            _PBtn.Press();
+            _PBtn.Press();
+            _TBtn.Press();
+            _SCBtn.Press();
+
+            _fakeOut.Received().OutputLine(Arg.Is<string>(str => str.Contains("100")));
+        }
+
+        [Test]
+        public void TurnOn_3PressOnPwrButton_PowerIs150()
+        {
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _TBtn.Press();
+            _SCBtn.Press();
+
+            _fakeOut.Received().OutputLine(Arg.Is<string>(str => str.Contains("150")));
 
         }
 
         [Test]
-        public void Test1()
+        public void TurnOn_8PressOnPwrButton_PowerIs400()
         {
-            Assert.Pass();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _TBtn.Press();
+            _SCBtn.Press();
+
+            _fakeOut.Received().OutputLine(Arg.Is<string>(str => str.Contains("400")));
+
         }
+
+
+        [Test]
+        public void TurnOn_14PressOnPwrButton_PowerIs700()
+        {
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+
+            _TBtn.Press();
+            _SCBtn.Press();
+
+            _fakeOut.Received().OutputLine(Arg.Is<string>(str => str.Contains("700")));            
+        }
+
+        [Test]
+        public void TurnOn_15PressOnPwrButton_PowerIs50()
+        {
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+            _PBtn.Press();
+
+            _TBtn.Press();
+            _SCBtn.Press();
+
+            _fakeOut.Received().OutputLine(Arg.Is<string>(str => str.Contains("50")));
+        }
+
     }
 }
