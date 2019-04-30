910fe10bde0252392ae533492c09bfac5869bcb8
Nikolaj Jensen <nikje94@gmail.com>
2019-04-30 20:03:56 +0200

    Done

--- tests/Microwave.Test.Integration/It03Sut-2.cs
+++ tests/Microwave.Test.Integration/It03Sut-2.cs
@@ -50,7 +50,7 @@ namespace Microwave.Test.Integration
         }
 
         [Test]
-        public void ShowPower_WasCalled_PowerButtonPressed()
+        public void PowerButtonPressed_SystemIsReady_CorrectOutput()
         {
             _btnPower.Press();
 
@@ -58,20 +58,124 @@ namespace Microwave.Test.Integration
         }
 
         [Test]
-        public void ShowTime_WasCalled_TimerButtonPressed()
+        public void PowerButtonPressedTwice_SystemIsReady_CorrectOutput()
         {
-            _btnTimer.Press();
+            _btnPower.Press();
+            _btnPower.Press();
 
-            _fakeOutput.Received().OutputLine($"Display shows: 00:01");
+            _fakeOutput.Received().OutputLine($"Display shows: 100 W");
         }
 
         [Test]
-        public void Clear_WasCalled_StartCancelButtonPressed()
+        public void PowerButtonPressed15Times_SystemIsReady_CorrectOutput()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                _btnPower.Press();
+
+            }
+
+            _fakeOutput.Received(2).OutputLine($"Display shows: 50 W");
+        }
+
+        [Test]
+        public void TimerButtonPressed_OnPowerSetup_CorrectOutput()
+        {
+            _btnPower.Press();
+            _btnTimer.Press();
+
+            _fakeOutput.Received().OutputLine($"Display shows: 01:00");
+        }
+
+        [Test]
+        public void TimerButtonPressedTwice_OnPowerSetup_CorrectOutput()
+        {
+            _btnPower.Press();
+            _btnTimer.Press();
+            _btnTimer.Press();
+
+            _fakeOutput.Received().OutputLine($"Display shows: 02:00");
+        }
+
+
+        [Test]
+        public void StartCancelButtonPressed_OnPowerSetup_DisplayClear()
         {
             _btnPower.Press();
             _btnStartCancel.Press();
 
             _fakeOutput.Received().OutputLine($"Display cleared");
         }
+
+        [Test]
+        public void StartCancelButtonPressed_WhileCooking_DisplayClear()
+        {
+            _btnPower.Press();
+            _btnTimer.Press();
+            _btnStartCancel.Press();
+
+            _btnStartCancel.Press();
+
+            _fakeOutput.Received(2).OutputLine($"Display cleared");
+        }
+
+        /*
+         * Fejlede, da OnTimerEvent trækker 1000 sek.
+         * fra TimeRemaining i stedet for 1 sek.
+         */
+        [Test]
+        public void StartedCooking_Wait1Second_CorrectOutput()
+        {
+            _btnPower.Press();
+            _btnTimer.Press();
+            _btnStartCancel.Press();
+
+            Thread.Sleep(1500); // make sure 1 second actually passes
+
+            _fakeOutput.Received().OutputLine("Display shows: 00:59");
+        }
+
+        [Test]
+        public void DoorOpened_OnPowerSetup_DisplayClear()
+        {
+            _btnPower.Press();
+            _door.Open();
+
+            _fakeOutput.Received().OutputLine($"Display cleared");
+        }
+
+        [Test]
+        public void DoorOpened_OnTimeSetup_DisplayClear()
+        {
+            _btnPower.Press();
+            _btnTimer.Press();
+            _door.Open();
+
+            _fakeOutput.Received().OutputLine($"Display cleared");
+        }
+
+        [Test]
+        public void DoorOpened_WhileCooking_DisplayClear()
+        {
+            _btnPower.Press();
+            _btnTimer.Press();
+            _btnStartCancel.Press();
+            _door.Open();
+
+            _fakeOutput.Received().OutputLine($"Display cleared");
+        }
+
+        [Test]
+        public void CookingIseDone_WhileCooking_DisplayClear()
+        {
+            _btnPower.Press();
+            _btnTimer.Press();
+            _btnStartCancel.Press();
+
+            _userInterface.CookingIsDone();
+
+            _fakeOutput.Received().OutputLine($"Display cleared");
+        }
+
     }
 }
