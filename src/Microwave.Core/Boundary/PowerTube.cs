d63bb8f401b0a07b5245e6d789d059905f0014b3
Mikkel O. Laursen <m@ikkel.dk>
2019-04-30 18:07:58 +0200

    Set power levels to be of unit watt

--- src/Microwave.Core/Boundary/PowerTube.cs
+++ src/Microwave.Core/Boundary/PowerTube.cs
@@ -16,7 +16,7 @@ namespace Microwave.Core.Boundary
 
         public void TurnOn(int power)
         {
-            if (power < 1 || 100 < power)
+            if (power < 50 || 700 < power)
             {
                 throw new ArgumentOutOfRangeException("power", power, "Must be between 1 and 100 % (incl.)");
             }
@@ -26,7 +26,7 @@ namespace Microwave.Core.Boundary
                 throw new ApplicationException("PowerTube.TurnOn: is already on");
             }
 
-            myOutput.OutputLine($"PowerTube works with {power} %");
+            myOutput.OutputLine($"PowerTube works with {power} watt");
             IsOn = true;
         }
 
