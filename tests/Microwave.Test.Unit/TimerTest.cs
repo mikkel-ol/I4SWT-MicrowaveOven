6812367e22bc182ce797430abb70491488f5778c
Mikkel O. Laursen <m@ikkel.dk>
2019-04-30 17:11:13 +0200

    Update unit test for Timer to be of seconds instead of milliseconds

--- tests/Microwave.Test.Unit/TimerTest.cs
+++ tests/Microwave.Test.Unit/TimerTest.cs
@@ -21,7 +21,7 @@ namespace Microwave.Test.Unit
             ManualResetEvent pause = new ManualResetEvent(false);
 
             uut.TimerTick += (sender, args) => pause.Set();
-            uut.Start(2000);
+            uut.Start(2);
 
             // wait for a tick, but no longer
             Assert.That(pause.WaitOne(1100));
@@ -35,7 +35,7 @@ namespace Microwave.Test.Unit
             ManualResetEvent pause = new ManualResetEvent(false);
 
             uut.TimerTick += (sender, args) => pause.Set();
-            uut.Start(2000);
+            uut.Start(2);
 
             // wait shorter than a tick, shouldn't come
             Assert.That(!pause.WaitOne(900));
@@ -49,7 +49,7 @@ namespace Microwave.Test.Unit
             ManualResetEvent pause = new ManualResetEvent(false);
 
             uut.Expired += (sender, args) => pause.Set();
-            uut.Start(2000);
+            uut.Start(2);
 
             // wait for expiration, but not much longer, should come
             Assert.That(pause.WaitOne(2100));
@@ -63,7 +63,7 @@ namespace Microwave.Test.Unit
             ManualResetEvent pause = new ManualResetEvent(false);
 
             uut.Expired += (sender, args) => pause.Set();
-            uut.Start(2000);
+            uut.Start(2);
 
             // wait shorter than expiration, shouldn't come
             Assert.That(!pause.WaitOne(1900));
@@ -80,7 +80,7 @@ namespace Microwave.Test.Unit
             uut.Expired += (sender, args) => pause.Set();
             uut.TimerTick += (sender, args) => notifications++;
 
-            uut.Start(2000);
+            uut.Start(2);
 
             // wait longer than expiration
             Assert.That(pause.WaitOne(2100));
@@ -88,6 +88,5 @@ namespace Microwave.Test.Unit
 
             Assert.That(notifications, Is.EqualTo(2));
         }
-
     }
 }
