87d618fad9a29356933cf20f859e9899860478d0
Mikkel O. Laursen <m@ikkel.dk>
2019-04-30 12:55:09 +0200

    Add IT02 tests

---
+++ tests/Microwave.Test.Integration/It02Sut-1.cs
@@ -0,0 +1,69 @@
+using System;
+using NUnit.Framework;
+using NSubstitute;
+using Microwave.Core.Boundary;
+using Microwave.Core.Interfaces;
+using Microwave.Core.Controllers;
+
+namespace Tests
+{
+    // System under test
+    // Iteration 02, Light
+    public class It02Sut1
+    {
+        private ICookController _fakeCookCtrl;
+        private IDisplay _fakeDisp;
+        private IOutput _fakeOut;
+
+        private Button _PBtn;
+        private Button _TBtn;
+        private Button _SCBtn;
+        private Door _Door;
+        private UserInterface _UI;
+
+        private Light _sutLight;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakeCookCtrl = Substitute.For<ICookController>();
+            _fakeDisp = Substitute.For<IDisplay>();
+            _fakeOut = Substitute.For<IOutput>();
+
+            _PBtn = new Button();
+            _TBtn = new Button();
+            _SCBtn = new Button();
+            _Door = new Door();
+
+            _sutLight = new Light(_fakeOut);
+
+            _UI = new UserInterface(_PBtn, _TBtn, _SCBtn, _Door, _fakeDisp, _sutLight, _fakeCookCtrl);
+        }
+
+        [TestCase(1, TestName = "OpenDoor StateReady ShouldCall OutputLine")]
+        public void TestTurnOn(int result)
+        {
+            var wasCalledCount = 0;
+
+            _Door.Opened += (o, e) => wasCalledCount++;
+            _Door.Open();
+
+            _fakeOut.Received(1).OutputLine("Light is turned on");  // Assert Output received this line
+            Assert.That(wasCalledCount, Is.EqualTo(result));        // Assert Door.Open() was called once
+        }
+
+        [TestCase(2, TestName = "OpenDoor CloseDoor ShouldCall OutputLine")]
+        public void TestTurnOff(int result)
+        {
+            var wasCalledCount = 0;
+
+            _Door.Opened += (o, e) => wasCalledCount++;
+            _Door.Closed += (o ,e) => wasCalledCount++;
+            _Door.Open();
+            _Door.Close();
+
+            _fakeOut.Received(1).OutputLine("Light is turned off");  // Assert Output received this line
+            Assert.That(wasCalledCount, Is.EqualTo(result));        // Assert Door.Open() and Door.Close() was called
+        }
+    }
+}
---
+++ tests/Microwave.Test.Integration/It02Sut-2.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Threading;
+using NUnit.Framework;
+using NSubstitute;
+using Microwave.Core.Boundary;
+using Microwave.Core.Interfaces;
+using Microwave.Core.Controllers;
+
+namespace Tests
+{
+    // System under test
+    // Iteration 02, Light
+    public class It02Sut2
+    {
+        private IDisplay _fakeDisp;
+        private IOutput _fakeOut;
+        private ITimer _fakeTimer;
+        private IPowerTube _fakePowerTube;
+
+        // Double dependency
+        private IUserInterface _fakeUI;
+        private UserInterface _UI;
+
+        private Button _PBtn;
+        private Button _TBtn;
+        private Button _SCBtn;
+        private Door _Door;
+        private Light _Light;
+
+        private CookController _sutCookCtrl;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakeDisp = Substitute.For<IDisplay>();
+            _fakeOut = Substitute.For<IOutput>();
+            _fakeTimer = Substitute.For<ITimer>();
+            _fakePowerTube = Substitute.For<IPowerTube>();
+            _fakeUI = Substitute.For<IUserInterface>();
+
+            _PBtn = new Button();
+            _TBtn = new Button();
+            _SCBtn = new Button();
+            _Door = new Door();
+            _Light = new Light(_fakeOut);
+
+            _sutCookCtrl = new CookController(_fakeTimer, _fakeDisp, _fakePowerTube, _fakeUI);
+
+            _UI = new UserInterface(_PBtn, _TBtn, _SCBtn, _Door, _fakeDisp, _Light, _sutCookCtrl);
+        }
+
+        [TestCase(50, 30, TestName = "StartCooking ShouldCall PowerTubeTurnOn And TimerStart")]
+        public void TestStartCooking(int power, int time)
+        {
+            _sutCookCtrl.StartCooking(power, time);
+
+            _fakePowerTube.Received(1).TurnOn(power);
+            _fakeTimer.Received(1).Start(time);
+        }
+
+        [TestCase(50, 5, TestName = "OnTimerExpired ShouldCall PowerTubeTurnOff And UICookingIsDone")]
+        public void TestOnTimerExpired(int power, int time)
+        {
+            _sutCookCtrl.StartCooking(power, time); // Set isCooking = true
+
+            _sutCookCtrl.OnTimerExpired(null, EventArgs.Empty);
+
+            _fakePowerTube.Received(1).TurnOff();
+            _fakeUI.Received(1).CookingIsDone();
+        }
+
+        [TestCase(60, TestName = "OnTimerTick FakeTimeRemaining ShouldCall DisplayShowTime")]
+        public void TestOnTimerTick(int timeRemaining)
+        {
+            _fakeTimer.TimeRemaining.Returns(timeRemaining);
+
+            _sutCookCtrl.OnTimerTick(null, EventArgs.Empty);
+
+            _fakeDisp.Received(1).ShowTime(timeRemaining/60, timeRemaining % 60);
+        }
+    }
+}
